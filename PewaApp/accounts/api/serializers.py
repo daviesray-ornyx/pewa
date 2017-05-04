e4fed4924cc5dbbdb211306f4630148916c2bfab
daviesray-ornyx <daviesray.ornyx@gmail.com>
2017-05-04 12:43:55 +0300

    With --all option

--- PewaApp/accounts/api/serializers.py
+++ PewaApp/accounts/api/serializers.py
@@ -1,48 +1,94 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model, authenticate
+from django.db.models import Q
 
-from PewaApp.models import Vendor, Product
+from rest_framework.serializers import (
+    EmailField,
+    CharField,
+
+    ModelSerializer
+)
+
+from rest_framework.validators import (
+    ValidationError
+)
+
+User = get_user_model()
 
 
-class VendorCreateSerializer(ModelSerializer):
+class CreateUserSerializer(ModelSerializer):
+    username = CharField(label='Username', trim_whitespace=True)
+    email = EmailField(label='Email Address', trim_whitespace=True)
+    password = CharField(label='Password', style={'input_type': 'password'}, trim_whitespace=True)
 
     class Meta:
-        model = Vendor
+        model = User
         fields = [
-            'name',
-            'phone_number',
-            'email_address']
+            'username',
+            'email',
+            'password'
+        ]
+        extra_kwargs = {"password":
+                            {"write_only": True}
+                        }
+
+    def validate_email(self, value):
+        user_qs = User.objects.filter(email=value)
+        if user_qs.exists():
+            raise ValidationError('A user with this email address already exist')
+        return value
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+            username=username,
+            email=email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data
 
 
-class VendorListSerializer(ModelSerializer):
-
-    class Meta:
-        model = Vendor
-        fields = ['name', 'slug', 'phone_number', 'email_address']
-
-
-class VendorDetailSerializer(ModelSerializer):
-
-    class Meta:
-        model = Vendor
-        fields = ['pk', 'name', 'slug', 'phone_number', 'email_address']
-"""
-data = {
-    'name': 'Douma Construction',
-    'phone_number': '0876567656',
-    'email_address': 'douma@gmail.com'
-}
-"""
-
-
-class ProductListSerializer(ModelSerializer):
-
-    class Meta:
-        model = Product
-        fields = ['name', 'slug', 'vendor', 'price']
-
-
-class ProductDetailSerializer(ModelSerializer):
+class LoginUserSerializer(ModelSerializer):
+    token = CharField(label='Token', required=False, allow_blank=True, allow_null=True,  read_only=True)
+    username = CharField(label='Username', required=False, trim_whitespace=True)
+    email = EmailField(label='Email Address', required=False, trim_whitespace=True)
+    password = CharField(label='Password', required=True, style={'input_type': 'password'}, trim_whitespace=True)
 
     class Meta:
-        model = Product
-        fields = ['pk', 'name', 'slug', 'vendor', 'price']
+        model = User
+        fields = [
+            'token',
+            'username',
+            'email',
+            'password'
+        ]
+        extra_kwargs = {"password":
+                            {"write_only": True}
+                        }
+
+    def validate(self, data):
+        user_obj = None
+        email = data.get('email', None)
+        username = data.get('username', None)
+        password = data['password']
+        if email is None and username is None:
+            raise ValidationError("A username or email is required to login.")
+        user = User.objects.filter(
+            Q(email=email) |
+            Q(username=username)
+        ).distinct().exclude(email__isnull=True)
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("This email/username is not valid.")
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError("Incorrect credentials. Please try again.")
+        else:
+            data['token'] = 'SOME RANDOM TOKEN'
+        return data
+
---
+++ PewaApp/accounts/api/views.py
@@ -0,0 +1,54 @@
+from __future__ import unicode_literals
+
+from django.db.models import Q
+from django.contrib.auth import get_user_model
+
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveUpdateAPIView
+)
+
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+
+
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+)
+
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter
+)
+
+from .serializers import (
+    CreateUserSerializer,
+    LoginUserSerializer
+)
+
+
+User = get_user_model()
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = CreateUserSerializer
+    queryset = User.objects.all()
+
+
+class UserLoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = LoginUserSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            new_data = serializer.data
+            return Response(new_data, status=HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
